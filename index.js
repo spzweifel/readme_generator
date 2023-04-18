b80fbc2302d8d441cd89f16430ad667d66d1acad
Sean Zweifel <spzweifel@gmail.com>
2023-04-18 18:42:25 -0400

    got started

---
+++ index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+// the inquirer package can allow us to ask questions of the user
+const inquirer = require('inquirer')
+// fs writes to the file system
+const fs = require ("fs")
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
